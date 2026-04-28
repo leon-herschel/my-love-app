01c2b61efe002952992be30b3b8e9926d5089a11
leon-herschel <onikleon24@gmail.com>
2026-04-28 16:31:57 +0800

    added image

--- src/components/FinalPage.jsx
+++ src/components/FinalPage.jsx
@@ -83,7 +83,7 @@ export default function FinalPage() {
         <div className="text-center animate-fade-in max-w-md">
 
             <img
-              src="/src/assets/us.jpg"
+              src="/us.jpg"
               alt="us"
               className="rounded-xl mb-5 w-full object-cover"
             />
